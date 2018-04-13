d2086b649e65cc3fb5716ba1afd2577b7262cb5b
pana83 <pavel_cristi2004@yahoo.com>
2018-04-13 17:36:12 +0200

    created bookENdpointTest

--- src/main/java/com/pluralsight/bookstore/rest/JAXRSConfiguration.java
+++ src/main/java/com/pluralsight/bookstore/rest/JAXRSConfiguration.java
@@ -3,6 +3,13 @@ package com.pluralsight.bookstore.rest;
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
+/**
+ * @author Antonio Goncalves
+ *         http://www.antoniogoncalves.org
+ *         --
+ */
+
 @ApplicationPath("api")
 public class JAXRSConfiguration extends Application {
+
 }
